fe027815c5599ddad4d4013e10f6444aa591435a
guxiaowu <337118043@qq.com>
2018-03-16 19:04:05 +0800

    注释

--- DFS/HDU-2553.cpp
+++ DFS/HDU-2553.cpp
@@ -11,22 +11,22 @@ int n, counter = 0;
 
 int a[MAXNUMBER];
 
-bool check(int x, int y) {
-	for (int i = 0; i < x; i++)
+bool check(int x, int y) {			//判断在x行，y列可以放置"queen"
+	for (int i = 0; i < x; i++)			
 		if (a[i] == y || a[i] - i == y - x || a[i] + i == y + x)
 			return false;
 	return true;
 
 }
 
-void dfs(int step) {
-	if (step == n - 1) 
+void dfs(int step) {	
+	if (step == n - 1) 				//最后一行可以放置即数目+1，递归结束
 		for (int i = 0; i < n; i++) 
-			if (check(step, i)) {
+			if (check(step, i)) {	
 				counter++;
 				return;
 			}
-	for (int i = 0; i<n; i++) {
+	for (int i = 0; i<n; i++) {		//dfs搜索
 		if (check(step, i) ){
 			a[step] = i;
 			dfs(step + 1);
@@ -41,7 +41,7 @@ int main()
 	scanf("%d", &n);
 	while (n != 0)
 	{
-		dfs(0);
+		dfs(0);					//从第一层开始搜索
 		printf("%d\n", counter);
 		counter = 0;
 		scanf("%d", &n);
